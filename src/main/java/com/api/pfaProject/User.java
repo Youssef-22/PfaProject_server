680fda191ad92bdf16ebb994e1b3757a4834bc94
ImadBoujhair <Imadboujhair125@gmail.com>
2024-03-25 12:35:06 +0000

    imad_firstcommit

--- src/main/java/com/api/pfaProject/User.java
+++ src/main/java/com/api/pfaProject/User.java
@@ -3,6 +3,8 @@ package com.api.pfaProject;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,19 +17,83 @@ public class User {
     @Id
     private String id;
 
+    @Getter @Setter
     private String firstName;
 
+    @Getter @Setter
     private String lastName;
 
+    @Getter @Setter
     private String username;
 
+    @Getter @Setter
     private String email;
 
+
+    @Getter @Setter
     private String password;
 
 
+    @Getter @Setter
     private String profilePictureUrl;
 
-//    public User(String firstName, String lastName, String username, String email, String password, MultipartFile file) {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfilePictureUrl() {
+        return profilePictureUrl;
+    }
+
+    public void setProfilePictureUrl(String profilePictureUrl) {
+        this.profilePictureUrl = profilePictureUrl;
+    }
+
+
+    //    public User(String firstName, String lastName, String username, String email, String password, MultipartFile file) {
 //    }
 }
